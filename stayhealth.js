9e52896c6728062196d31dc9fe5d1a480082dd84
manhmayman <qtvc01@gmail.com>
2022-12-03 17:10:26 +0700

    test

---
+++ stayhealth.js
@@ -0,0 +1,81 @@
+function change1() {
+    let x = document.getElementsByClassName("img");
+    x[0].src = 'https://vinmec-prod.s3.amazonaws.com/images/20200311_022520_975923_phomai.max-1800x1800.jpg';
+    x[1].src = "https://vinmec-prod.s3.amazonaws.com/images/20190927_181939_318142_sua-chua-ngua-ung-t.max-1800x1800.jpg";
+    x[2].src = "https://cdn.tgdd.vn/2021/11/CookDish/hanh-nhan-la-gi-8-tac-dung-than-ky-cua-hanh-nhan-doi-voi-suc-avt-1200x676.jpg";
+    x[3].src =  "http://cdn.tgdd.vn/Files/2019/01/15/1144810/13-loai-hat-bo-cho-suc-khoe-ma-ban-nen-biet-bach-hoa-xanh-202109281312349529.jpg";
+
+    let y = document.getElementsByClassName("textt")
+    y[0].innerHTML = 'Hầu hết các loại phô mai đều là nguồn thực phẩm giàu canxi tuyệt vời';
+    y[1].innerHTML = 'Sữa chua là vừa một nguồn canxi tuyệt vời, vừa giàu chủng men vi sinh sống, mang lại nhiều lợi ích cho sức khỏe.';
+    y[2].innerHTML = 'Trong tất cả các loại hạt, hạnh nhân là một trong những loại giàu canxi nhất';
+    y[3].innerHTML = 'Nhiều loại hạt có kích thước nhỏ bé nhưng rất giàu dinh dưỡng';
+
+    let z = document.getElementsByClassName("pp")
+    z[0].innerHTML = "Phô mai cung cấp khoảng 5 - 20% nhu cầu canxi hàng ngày trên mỗi khẩu phần 28g."
+    z[1].innerHTML = "Một cốc (245g) sữa chua nguyên chất chứa 30% nhu cầu canxi, cộng thêm phốt pho, kali và vitamin B2 và B12."
+    z[2].innerHTML = "Trong 28g hạnh nhân cũng chứa 3g chất xơ, chất béo và protein lành mạnh"
+    z[3].innerHTML = "Trong đó, vừng và hạt chia là hai loại quen thuộc, không chỉ chứa nhiều canxi, mà còn cung cấp protein"
+
+}
+
+function change2() {
+    let a = document.getElementsByClassName("img");
+    a[0].src = " https://vinmec-prod.s3.amazonaws.com/images/20200729_051027_700925_Lycopene-6.max-800x800.jpg"
+    a[1].src = "https://file.hstatic.net/200000240163/article/ca_rot_huu_co_b4b636bd8ac5431db8b3f8957634d14d_1024x1024.jpg"
+    a[2].src = "https://suckhoedoisong.qltns.mediacdn.vn/324455921873985536/2021/10/20/cong-dung-cua-bi-ngo-1-16347456740811547198272.jpg"
+    a[3].src = "https://vinmec-prod.s3.amazonaws.com/images/20210516_113632_727988_an-khoai-lang-moi-n.max-1800x1800.jpg"
+
+    let b = document.getElementsByClassName("textt");
+    b[0].innerHTML = "Cà chua chứa ít calo và nhiều khoáng chất và cũng là nguồn dưỡng chất vitamin A dồi dào."
+    b[1].innerHTML = "Các vitamin A trong cà rốt có ở dạng beta-carotene, một chất chống oxy hóa lành mạnh."
+    b[2].innerHTML = "Bi Ngo dù sống hoặc được nấu chín, bí ngô đều một món ăn rất thơm ngon và bổ dưỡng"
+    b[3].innerHTML = "Khoai lang được nhiều người yêu thích vì hương vị ngon và hàm lượng dinh dưỡng cao. Khoai lang được cho là thực phẩm chứa vitamin A dồi dào nhất."
+
+    let c = document.getElementsByClassName("pp");
+    c[0].innerHTML = "Một quả cà chua trung bình sẽ cung cấp khoảng 20% lượng nhu cầu vitamin A trong ngày."
+    c[1].innerHTML = "Cà rốt cũng chứa một lượng lớn vitamin B, C, K, chất xơ và magiê. "
+    c[2].innerHTML = " Loại quả này chứa nhiều chất beta-carotene, được chuyển đổi thành vitamin A trong cơ thể.  Bí ngô cũng là một nguồn thực phẩm chứa vitamin C, magiê và chất xơ."
+    c[3].innerHTML = "Khoai lang được nhiều người yêu thích vì hương vị ngon và hàm lượng dinh dưỡng cao. Khoai lang được cho là thực phẩm chứa vitamin A dồi dào nhất."
+}
+
+function change3() { 
+    let v = document.getElementsByClassName("img");
+    v[0].src = " http://cdn.tgdd.vn/Files/2018/01/24/1061437/cai-be-xanh-chua-benh-mua-lanh-ban-da-biet-202110271122566003.jpg"
+    v[1].src = "https://tieudung.kinhtedothi.vn/upload_images/images/2021/07/29/dau-trang.jpg"
+    v[2].src = "https://vcdn1-suckhoe.vnecdn.net/2022/06/14/avocado-products-made-from-avo-2274-9899-1655197911.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=EWOaBBrQi2HZnmDaVp5uYg"
+    v[3].src = "http://cdn.tgdd.vn/Files/2021/01/19/1321272/tong-hop-nhung-cach-che-bien-ca-hoi-ngon-bo-duong-202112282310178876.jpg"
+
+    let n = document.getElementsByClassName("textt");
+    n[0].innerHTML = "Cải bẹ xanh có lượng calorie thấp nhưng lại nhiều chất xơ cùng các vitamin và khoáng chất thiết yếu."
+    n[1].innerHTML = "Đậu trắng là một loại thực phẩm giàu chất dinh dưỡng vì chứa nhiều chất xơ và protein."
+    n[2].innerHTML = "Bơ là loại quả rất bổ dưỡng cho cơ thể, nó chứa nhiều loại chất dinh dưỡng, bao gồm 20 loại vitamin cùng khoáng chất như: vitamin C, E, K và B6,... b"
+    n[3].innerHTML = "Cá hồi được biết đến là loại cá có vô vàn chất dinh dưỡng tốt cho sức khỏe nó chứa omega3,protein..."
+
+    let m = document.getElementsByClassName("pp");
+    m[0].innerHTML = "Cải bẹ xanh-chúng là nguồnCà rố cung cấp vitamin C và K dồi dào."
+    m[1].innerHTML = "Chúng chứa rất ít chất béo, không có cholesterol và có cùng lượng canxi như một ly sữa đặc biết rất giàu Kali. "
+    m[2].innerHTML = "Chất dinh dưỡng này giúp duy trì các gradient điện trong tế bào của cơ thể và phục vụ các chức năng quan trọng khác nhau."
+    m[3].innerHTML = "Cá Hồi cũng là nguồn thực phẩm cung cấp vitamin B12 tốt nhất cùng với kali và các chất dinh dưỡng khác như sắt và vitamin D."
+}
+
+function change4() {
+    let j = document.getElementsByClassName("img");
+    j[0].src = "https://image.thanhnien.vn/w660/Uploaded/2022/lxwpcqjwp/2020_04_14/sua1-nb2_pzcb.jpg"
+    j[1].src = "https://cdn.tgdd.vn/2021/06/CookProduct/1200-1200x676-1.jpg"
+    j[2].src = "https://tepbac.com/upload/species/ge_image/2021/06/tom-su-30-con-ao_1622970972.jpeg"
+    j[3].src = "https://vinmec-prod.s3.amazonaws.com/images/20200506_012704_682187_bong-cai-xanh.max-1800x1800.jpg"
+
+    let k = document.getElementsByClassName("textt");
+    k[0].innerHTML = "Sữa chứa gần như mọi chất dinh dưỡng mà cơ thể bạn cần"
+    k[1].innerHTML = "Trứng là một trong những thực phẩm lành mạnh và bổ dưỡng nhất hiện nay"
+    k[2].innerHTML = "Tôm là một loại hải sản có hàm lượng calo thấp nhưng chứa nhiều chất dinh dưỡng"
+    k[3].innerHTML = "Bông cải xanh nó có hàm lượng protein cao"
+
+    let l = document.getElementsByClassName("pp");
+    l[0].innerHTML = "Đây là nguồn protein chất lượng cao và có nhiều canxi, phốt pho và riboflavin (vitamin B2)"
+    l[0].innerHTML = "Đây là nguồn dinh dưỡng tuyệt vời do có nhiều vitamin, khoáng chất, chất béo lành mạnh, chất chống oxy hóa bảo vệ mắt và các chất dinh dưỡng cho não"
+    l[0].innerHTML = "Tôm bao gồm selen và vitamin B12. Giống như cá, tôm chứa axit béo omega-3"
+    l[0].innerHTML = "Bông cải xanh là một loại rau lành mạnh cung cấp vitamin C, vitamin K, chất xơ và kali. Nó cũng cung cấp các chất dinh dưỡng có hoạt tính sinh học giúp bảo vệ cơ thể chống lại ung thư."
+    
+}
